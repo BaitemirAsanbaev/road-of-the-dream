567dd06519f5b62b796e6ff1d3fe5cbb2a4243ed
darkbala <rustam.samsaev@gmail.com>
2023-05-05 23:36:23 +0600

    html

--- src/components/Map/LocationPicker2.js
+++ src/components/Map/LocationPicker2.js
@@ -1,52 +1,43 @@
-import React, { useState } from "react";
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  useMapEvents,
-  Polyline,
-} from "react-leaflet";
-import L from 'leaflet';
+// import React, { useState } from 'react';
+// import { MapContainer, TileLayer, Marker, useMapEvents, Polyline } from 'react-leaflet';
+// import L from 'leaflet';
 
-import marker_icon from "../assets/marker.svg"
-const customIcon = L.icon({
-    iconUrl: marker_icon,
-    iconSize: [32, 32],
-  });
-  
-const LocationPicker2 = () => {
-  const [positions, setPositions] = useState([]);
-  const [badRoads, setBadRoads] = useState([]);
+// import marker_icon from '../../assets/marker.svg';
 
-  const handleMapClick = (e) => {
-    const { lat, lng } = e.latlng;
-    const newPosition = [lat, lng];
-    setPositions((prevPositions) => [...prevPositions, newPosition]);
-    setBadRoads((prevBadRoads) => [...prevBadRoads, newPosition]);
-    console.log(badRoads);
-  };
+// const customIcon = L.icon({
+//   iconUrl: marker_icon,
+//   iconSize: [32, 32],
+// });
 
-  const LocationMarkers = () => {
-    useMapEvents({
-      click: handleMapClick,
-    });
+// const LocationPicker2 = () => {
+//   const [positions, setPositions] = useState([]);
+//   const [badRoads, setBadRoads] = useState([]);
 
-    return positions.map((position, index) => (
-      <Marker key={index} position={position} icon={customIcon} />
-    ));
-  };
+//   const handleMapClick = (e) => {
+//     const { lat, lng } = e.latlng;
+//     const newPosition = [lat, lng];
+//     setPositions((prevPositions) => [...prevPositions, newPosition]);
+//     setBadRoads((prevBadRoads) => [...prevBadRoads, newPosition]);
+//     console.log(badRoads);
+//   };
 
-  return (
-    <MapContainer
-      center={[42.8746, 74.5698]}
-      zoom={13}
-      style={{ height: "400px" }}
-    >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <LocationMarkers />
-      <Polyline pathOptions={{ color: "red" }} positions={badRoads} />
-    </MapContainer>
-  );
-};
+//   const LocationMarkers = () => {
+//     useMapEvents({
+//       click: handleMapClick,
+//     });
 
-export default LocationPicker2;
+//     return positions.map((position, index) => (
+//       <Marker key={index} position={position} icon={customIcon} />
+//     ));
+//   };
+
+//   return (
+//     <MapContainer center={[42.8746, 74.5698]} zoom={13} style={{ width: '300px', height: '300px' }}>
+//       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+//       <LocationMarkers />
+//       <Polyline pathOptions={{ color: 'red' }} positions={badRoads} />
+//     </MapContainer>
+//   );
+// };
+
+// export default LocationPicker2;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,56 @@
+import Header from '../Header/Header';
+import sexy from './Home.module.scss';
+import Rating from '@mui/material/Rating';
+import send from '../../assets/arrow-upward.png';
+
 const Home = () => {
-    return ( <div>
-        <h1>Home</h1>
-    </div> );
-}
- 
+  return (
+    <div>
+      <Header />
+      <div className={sexy.homeWrapper}>
+        <div className={sexy.homeForm}>
+          <div className={sexy.map}>
+            <h3>leaflet map</h3>
+          </div>
+          <form className={sexy.form}>
+            <h1>Заполнение жалобы</h1>
+            <div className={sexy.reason}>
+              <input
+                type="text"
+                className={sexy.reasonInput}
+                placeholder="Введите причину жалобы"
+              />
+            </div>
+            <div className={sexy.strName}>
+              <input
+                type="text"
+                className={sexy.streetInput}
+                placeholder="Введите название улицы"
+              />
+            </div>
+            <div className={sexy.star}>
+              <p>
+                Оцените качество дороги: <br />
+                oт 1 до 5 (1- норм 5- слишком плохо)
+              </p>
+              <Rating name="size-large" defaultValue={2} size="large" className={sexy.rating} />
+            </div>
+            <div className={sexy.photo}>
+              <p>Загрузите фото:</p>
+              <input type="file" className={sexy.file} />
+            </div>
+            <div className={sexy.area}>
+              <textarea placeholder="Опишите проблему подробнее" className={sexy.desc}></textarea>
+            </div>
+            <button type="submit" className={sexy.send}>
+              Отправить
+              <img src={send} alt="send" className={sexy.sendPng} />
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
